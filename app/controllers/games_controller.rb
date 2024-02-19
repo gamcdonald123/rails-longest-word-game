deb7ac925bab5e1337f72754493ef8adcfbbd4bf
Guy McDonald <guy@goodfishgroup.com>
2024-02-19 16:50:32 +0000

    Got to the point where result is passing the write thing. Need to finish score.html.erb now.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,22 +10,39 @@ class GamesController < ApplicationController
 
   def score
 
+
+    @final_score = 0
     # We want to handle three scenarios:
     #   The word can’t be built out of the original grid ❌
-
-
+    grid = params[:letters].split
+    word = params[:word].upcase
+    answer_array = word.split('')
+    answer_array.each do |letter|
+      if grid.include?(letter)
+        grid.delete_at(grid.index(letter))
+        @final_score = 1
+      else
+        @result = "Sorry, but #{word} can't be built out of #{params[:letters]}"
+        @final_score = 0
+        break
+      end
+    end
     #   The word is valid according to the grid, but is not a valid English word ❌
     #   The word is valid according to the grid and is an English word ✅
-    #   binding.break
-    word = params[:word]
 
     url = "https://wagon-dictionary.herokuapp.com/#{word}"
 
     data = JSON.parse(URI.open(url).read)
 
-    # if (data[:found] == true) &&
+    if (data[:found] == true) && (@final_score == 1)
+      @result = "Congratulations! #{word} is a valid English word!"
+    elsif (data[:found] == false) && (@final_score == 1)
+      @result = "Sorry, but #{word} is not a valid English word"
+    elsif @final_score.zero?
+      @result = "Sorry, but #{word} can't be built out of #{params[:letters]}"
+    end
 
-    # else
-    # end
+    # raise
+
   end
 end
